8379420c8e1d958f4b1ec17046653619ea70dfb8
ksargent <kevin_sargent@student.uml.edu>
2017-02-10 09:25:12 -0500

    Created D Vector Script
    Script that uses the magnetic field and angular momentum to calculate
    the dipole needed to generate a torque that is equal and opposite to
    component vector of the angular momentum that is perpendicular to the
    magnetic field vector.

---
+++ Cleaned Up Files/Current_Setup/KS/Simulation_20170104/findDVector.m
@@ -0,0 +1,22 @@
+function D = findDVector(B,L)
+%Using the magnetic field (B) vector and angular momentum (L) vector, 
+%calculates the dipole (D) vector needed to generate a torque equal to the
+%negative of the component vector of L perpendicular to B
+
+    %find the unit vector in the B direction
+    Bunit = B/norm(B);
+    %find component of L perpendicular to B
+    Lp = L - dot(B,L)/(norm(B))^2*B;
+    %find the unit vector in the Lp direction
+    Lpunit = Lp/norm(Lp);
+    
+    %find unit vector in D direction
+    Dunit = cross(Lpunit, B);
+    
+    %find the magnitude of the D vector
+    Dnorm = norm(Lp)/norm(B);
+    
+    %create the D vector
+    D = Dnorm*Dunit;
+    
+end
